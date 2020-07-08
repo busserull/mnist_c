57394cdbefd30e6706ab6484197cc5aa9b7888fc
Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>
2020-07-08 10:26:13 +0200

    refactor backpropagation
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@ntnu.no>

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "mnist.h"
 #include "network.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 void set_w(Network *, int, Matrix);
@@ -11,35 +12,79 @@ double sigmoid(double v){
     return 1.0 / (1.0 + exp(-v));
 }
 
+double sigmoid_prime(double v){
+    double raised = exp(-v);
+    return raised / ((1 + raised) * (1 + raised));
+}
+
+void evaluate(const Network network, const MnistSet set){
+    /* double correct = 0; */
+    /* double total = 0; */
+
+    for(int i = 0; i < set.size; i++){
+        Matrix image = mnist_vectorize_image(set.images[i]);
+        Matrix guess = network_feed(network, image);
+
+        mnist_print_image(set.images[i]);
+        printf("Correct: %d, Guess: ", set.labels[i]);
+
+        printf("\n");
+        matrix_print(guess);
+        /* int max_index = 0; */
+        /* double max_guess = matrix_get(guess, 0, 0); */
+        /* for(int y = 0; y < guess.y; y++){ */
+        /*     if(matrix_get(guess, 0, y) > max_guess){ */
+        /*         max_guess = matrix_get(guess, 0, y); */
+        /*         max_index = y; */
+        /*     } */
+        /* } */
+        /* printf("%d (%.2f)\n", max_index, max_guess); */
+
+        matrix_delete(&image);
+        matrix_delete(&guess);
+    }
+}
+
 int main(){
-    int layers[] = {2, 3, 1};
-    Network network = network_new(3, layers, sigmoid, NULL);
+    srand(0);
 
-    Matrix A = matrix_new(3, 2);
-    matrix_set(A, 0, 0, 1);
-    matrix_set(A, 0, 1, 2);
-    matrix_set(A, 1, 0, 3);
-    matrix_set(A, 1, 1, 4);
-    matrix_set(A, 2, 0, 5);
-    matrix_set(A, 2, 1, 6);
+    MnistSet training_set, test_set;
+    mnist_new(&training_set, &test_set);
+    int layers[] = {784, 15, 15, 10};
+    Network network = network_new(4, layers, sigmoid, sigmoid_prime);
 
-    Matrix At = matrix_transpose(A);
+    int old_size = test_set.size;
+    printf("--- Before training ---\n");
+    test_set.size = 5;
+    evaluate(network, test_set);
+    test_set.size = old_size;
 
-    matrix_print(A);
-    matrix_print(At);
-    matrix_inplace_scale(At, 2.0);
-    matrix_print(At);
+    for(int epoch = 0; epoch < 10; epoch++){
+        Matrix * images = (Matrix *)malloc(100 * sizeof(Matrix));
+        Matrix * labels = (Matrix *)malloc(100 * sizeof(Matrix));
+        for(int i = 0; i < 100; i++){
+            images[i] = mnist_vectorize_image(training_set.images[i]);
+            labels[i] = mnist_vectorize_label(training_set.labels[i]);
+        }
 
-    matrix_delete(&A);
-    matrix_delete(&At);
+        network_learn(network, images, labels, 100, 0.3);
+
+        for(int i = 0; i < 100; i++){
+            matrix_delete(images + i);
+            matrix_delete(labels + i);
+        }
+        free(images);
+        free(labels);
+    }
+
+    printf("--- After training ---\n");
+    test_set.size = 5;
+    evaluate(network, test_set);
+    test_set.size = old_size;
 
     network_delete(&network);
-
-    /* MnistSet training_set, test_set; */
-    /* mnist_new(&training_set, &test_set); */
-
-    /* mnist_delete(&training_set); */
-    /* mnist_delete(&test_set); */
+    mnist_delete(&training_set);
+    mnist_delete(&test_set);
 
     return 0;
 }
--- network.c
+++ network.c
@@ -15,64 +15,63 @@ static void network_backpropagate(
     const Network network,
     const Matrix input,
     const Matrix correct_output,
-    Matrix ** p_nabla_weights,
-    Matrix ** p_nabla_biases
+    Matrix * delta_nabla_w,
+    Matrix * delta_nabla_b
 ){
-    Matrix * kernels = (Matrix *)malloc((network.layers - 1) * sizeof(Matrix));
-    Matrix * acts = (Matrix *)malloc(network.layers * sizeof(Matrix));
+    int nabla_size = network.layers - 1;
+    Matrix * kernels = (Matrix *)malloc(nabla_size * sizeof(Matrix));
+    Matrix * acts = (Matrix *)malloc((nabla_size + 1) * sizeof(Matrix));
 
-    acts[0] = matrix_deep_copy(input);
-    Matrix prev = matrix_deep_copy(input);
+    Matrix act = matrix_deep_copy(input);
+    acts[0] = act;
 
     /* Feed forward */
-    for(int i = 0; i < network.layers - 1; i++){
-        Matrix kernel = matrix_dot(network.weights[i], prev);
+    for(int i = 0; i < nabla_size; i++){
+        Matrix kernel = matrix_dot(network.weights[i], act);
         matrix_inplace_add(kernel, network.biases[i]);
         kernels[i] = kernel;
 
-        Matrix act = matrix_deep_copy(kernel);
+        act = matrix_deep_copy(kernel);
         matrix_inplace_apply(act, network.activation_function);
         acts[i + 1] = act;
-
-        matrix_delete(&prev);
-        prev = act;
     }
 
     /* Propagate backward */
-    Matrix kernel_prime = matrix_deep_copy(kernels[network.layers - 2]);
+    Matrix error = matrix_deep_copy(acts[nabla_size]);
+    matrix_inplace_sub(error, correct_output); // Cost derivative
+
+    Matrix kernel_prime = matrix_deep_copy(kernels[nabla_size - 1]);
     matrix_inplace_apply(kernel_prime, network.activation_prime);
-    Matrix error = matrix_deep_copy(acts[network.layers - 1]);
-    matrix_inplace_sub(error, correct_output);
     matrix_inplace_hadamard(error, kernel_prime);
-
-    Matrix act_t = matrix_transpose(acts[network.layers - 2]);
-    *p_nabla_biases[network.layers - 1] = error;
-    *p_nabla_weights[network.layers - 1] = matrix_dot(error, act_t);
-
     matrix_delete(&kernel_prime);
-    matrix_delete(&act_t);
 
-    for(int i = network.layers - 2; i >= 0; i--){
+    Matrix a_t = matrix_transpose(acts[nabla_size - 1]);
+    delta_nabla_w[nabla_size - 1] = matrix_dot(error, a_t);
+    delta_nabla_b[nabla_size - 1] = error;
+    matrix_delete(&a_t);
+
+    for(int i = nabla_size - 2; i >= 0; i--){
         kernel_prime = matrix_deep_copy(kernels[i]);
         matrix_inplace_apply(kernel_prime, network.activation_prime);
-        Matrix weight_t = matrix_transpose(network.weights[i + 1]);
-        Matrix error_prop = matrix_dot(weight_t, error);
-        matrix_inplace_hadamard(error_prop, kernel_prime);
 
-        act_t = matrix_transpose(acts[i - 1]);
-        *p_nabla_biases[i] = error_prop;
-        *p_nabla_weights[i] = matrix_dot(error_prop, act_t);
+        Matrix w_t = matrix_transpose(network.weights[i + 1]);
+        error = matrix_dot(w_t, error);
+        matrix_inplace_hadamard(error, kernel_prime);
+
+        a_t = matrix_transpose(acts[i]);
+        delta_nabla_w[i] = matrix_dot(error, a_t);
+        delta_nabla_b[i] = error;
 
         matrix_delete(&kernel_prime);
-        matrix_delete(&weight_t);
-        matrix_delete(&act_t);
+        matrix_delete(&w_t);
+        matrix_delete(&a_t);
     }
 
     /* Cleanup */
-    for(int i = 0; i < network.layers - 2; i++){
+    for(int i = 0; i < nabla_size; i++){
         matrix_delete(kernels + i);
     }
-    for(int i = 0; i < network.layers - 1; i++){
+    for(int i = 0; i < nabla_size + 1; i++){
         matrix_delete(acts + i);
     }
     free(kernels);
@@ -97,6 +96,9 @@ Network network_new(
         int to = layer_sizes[i + 1];
         network.weights[i] = matrix_new(to, from);
         network.biases[i] = matrix_new(to, 1);
+
+        matrix_inplace_scramble(network.weights[i]);
+        matrix_inplace_scramble(network.biases[i]);
     }
 
     return network;
@@ -123,34 +125,46 @@ void network_learn(
     int mini_batch_size,
     double learning_rate
 ){
-    Matrix * nabla_weights = (Matrix *)malloc(network.layers * sizeof(Matrix));
-    Matrix * nabla_biases = (Matrix *)malloc(network.layers * sizeof(Matrix));
-    Matrix * delta_nabla_w = (Matrix *)malloc(network.layers * sizeof(Matrix));
-    Matrix * delta_nabla_b = (Matrix *)malloc(network.layers * sizeof(Matrix));
+    int nabla_size = network.layers - 1;
+    Matrix * nabla_w = (Matrix *)malloc(nabla_size * sizeof(Matrix));
+    Matrix * nabla_b = (Matrix *)malloc(nabla_size * sizeof(Matrix));
+    for(int i = 0; i < nabla_size; i++){
+        nabla_w[i] = matrix_zero_from(network.weights[i]);
+        nabla_b[i] = matrix_zero_from(network.biases[i]);
+    }
+    Matrix * delta_nabla_w = (Matrix *)malloc(nabla_size * sizeof(Matrix));
+    Matrix * delta_nabla_b = (Matrix *)malloc(nabla_size * sizeof(Matrix));
 
     for(int i = 0; i < mini_batch_size; i++){
         network_backpropagate(
             network,
             mini_batch[i],
             mini_batch_labels[i],
-            &delta_nabla_w,
-            &delta_nabla_b
+            delta_nabla_w,
+            delta_nabla_b
         );
         for(int j = 0; j < network.layers - 1; j++){
-            matrix_inplace_add(nabla_weights[j], delta_nabla_w[j]);
-            matrix_inplace_add(nabla_biases[j], delta_nabla_b[j]);
+            matrix_inplace_add(nabla_w[j], delta_nabla_w[j]);
+            matrix_inplace_add(nabla_b[j], delta_nabla_b[j]);
+            matrix_delete(delta_nabla_w + j);
+            matrix_delete(delta_nabla_b + j);
         }
     }
 
     for(int i = 0; i < network.layers - 1; i++){
-        matrix_inplace_scale(nabla_weights[i], learning_rate);
-        matrix_inplace_scale(nabla_biases[i], learning_rate);
-        matrix_inplace_sub(network.weights[i], nabla_weights[i]);
-        matrix_inplace_sub(network.biases[i], nabla_biases[i]);
+        matrix_inplace_scale(nabla_w[i], learning_rate);
+        matrix_inplace_scale(nabla_b[i], learning_rate);
+        matrix_inplace_sub(network.weights[i], nabla_w[i]);
+        matrix_inplace_sub(network.biases[i], nabla_b[i]);
     }
 
-    free(nabla_weights);
-    free(nabla_biases);
+    /* Cleanup */
+    for(int i = 0; i < nabla_size; i++){
+        matrix_delete(nabla_w + i);
+        matrix_delete(nabla_b + i);
+    }
+    free(nabla_w);
+    free(nabla_b);
     free(delta_nabla_w);
     free(delta_nabla_b);
 }
